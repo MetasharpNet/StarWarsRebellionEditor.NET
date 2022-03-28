ccc83392b98cf55bbf555d50823c9f2bfe4ba756
Audric THEVENET <metasharp@gmail.com>
2022-03-28 17:06:50 +0200

    identifiers, new CMUNALTB.DAT file structure

--- SwRebellionEditor/GData/CMUNALTB.cs
+++ SwRebellionEditor/GData/CMUNALTB.cs
@@ -2,44 +2,37 @@
 {
     public class CMUNALTB : DatFile
     {
-        public uint UnknownHeader1; // 1
+        public uint Field1_One; // 1
         [ArraySize]
-        public uint CapitalShipsCount; // 12
-        public uint FamilyIdHeader; // 20
+        public uint UnitsGroupsCount; // 12
+        public uint FamilyId; // 20
         public byte[] SeedFamilyTableEntry = new byte[20]; // "SeedFamilyTableEntry" as 1 byte per char
-        public CMUNALTB_CapitalShip[] CapitalShips;
+        public CMUNALTB_UnitsGroup[] UnitsGroups;
     }
-    public class CMUNALTB_CapitalShip
+    public class CMUNALTB_UnitsGroup
     {
-        public uint unknown1; // index based 1
-        public uint unknown2; 
-        public uint percChance;
-        public uint unknown4;
-        public uint unknown5;
+        public uint Index; // index based 1
+        public uint Field2_One; // 1
+        public uint Probability; // 1, 9, 13, 27, 36, 40, 54, 58, 89, 90, 93, 97
+        public uint Field4_One; // 1
+        public uint Field5_One; // 1
         [ArraySize]
-        public uint CargosCount;
-        public CMUNALTB_Cargo[] Cargos;
-    }
-    public class CMUNALTB_Cargo
-    {
-        public uint RecNum; // 1
-        public uint RecType; // 0
-        public ushort Unknown3; // index based 0
-        public byte Unknown4; // 0
-        public byte CargoType; // 16:Troop, 28:Fighter, 60: SpecOp
+        public uint UnitsCount;
+        public CMUNALTB_Unit[] Units;
         public override string ToString()
         {
-            switch (CargoType)
-            {
-                case 16: // 0x10
-                    return "Troop";
-                case 28: // 0x1C
-                    return "Fighter";
-                case 60: // 0x3C
-                    return "SpecOp";
-                default:
-                    return "";
-            }
+            return String.Join<CMUNALTB_Unit>(",", Units);
+        }
+
+    }
+    public class CMUNALTB_Unit
+    {
+        public uint Field1_One; // 1
+        public uint Field2_Zero; // 0
+        public uint Unit;
+        public override string ToString()
+        {
+            return Enum.GetName(typeof(Identifiers), Unit);
         }
     }
 }
