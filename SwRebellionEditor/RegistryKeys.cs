f124aef158f48133a545371a6aea715a1cc842ab
Audric THEVENET <metasharp@gmail.com>
2023-11-06 17:17:29 +0100

    bad registry management

--- SwRebellionEditor/RegistryKeys.cs
+++ SwRebellionEditor/RegistryKeys.cs
@@ -9,18 +9,27 @@ public class RegistryKeys
     {
         while (!File.Exists(Path.Combine(InstalledLocation, "REBEXE.EXE")))
         {
+            var askPath = false;
             try
             {
                 _registrykey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\LucasArts Entertainment Company LLC\\Star Wars Rebellion");
                 InstalledLocation = GetValue<string>("InstalledLocation");
+                if (String.IsNullOrWhiteSpace(InstalledLocation))
+                    askPath = true;
             }
             catch (Exception ex)
+            {
+                askPath = true;
+            }
+            if (askPath)
             {
                 var openFolderDialog = new FolderBrowserDialog();
                 openFolderDialog.Description = "Please select the folder where your Star Wars Rebellion game is installed.";
                 openFolderDialog.InitialDirectory = "C:\\";
                 if (openFolderDialog.ShowDialog() == DialogResult.OK)
                     InstalledLocation = openFolderDialog.SelectedPath;
+                else
+                    Environment.Exit(0);
             }
         }
     }
